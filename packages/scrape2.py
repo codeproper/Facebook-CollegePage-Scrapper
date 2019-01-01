825565d390e9a7b9c7176a828d16c4c743fb1ac9
Rabin Joshi <41543258+rabeenj007@users.noreply.github.com>
2019-01-01 18:02:35 +0545

    Sorting tab space inconsistency

--- packages/scrape2.py
+++ packages/scrape2.py
@@ -53,30 +53,30 @@ def login(username,password):
             for row in urlreader:
                 browser.get(row[0])
                 sleep(10)
-                try:
-                    rating=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[1]/div/a/div/div[2]/div/span[1]').get_attribute("innerText")
-                except NoSuchElementException:
-					rating="pattern error"
-				try:	
-					likes=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div').get_attribute("innerText")
-				except NoSuchElementException:
-					likes="pattern error"
-				try:
-					follow=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div[1]/div[3]/div/div[2]/div').get_attribute("innerText")
-                except NoSuchElementException:
-					follow="pattren error"
-				try:	
-					address=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div[2]/div[3]/div/div[2]/div[1]').get_attribute("innerText")
-                except NoSuchElementException:    
-					address="pattern error"
-				try:	
-					contact=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div[2]/div[4]/div/div[2]/div').get_attribute("innerText")
-                except NoSuchElementException:    
-					contact="pattern error"
-				try:
-					clgname=browser.find_element_by_class_name('_64-f').get_attribute("innerText")
-                except NoSuchElementException:
-                    clgname="pattern error"
+		try:
+			rating=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[1]/div/a/div/div[2]/div/span[1]').get_attribute("innerText")
+		except NoSuchElementException:
+			rating="pattern error"
+		try:
+			likes=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div').get_attribute("innerText")
+		except NoSuchElementException:
+			likes="pattern error"
+		try:
+			follow=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div[1]/div[3]/div/div[2]/div').get_attribute("innerText")
+		except NoSuchElementException:
+			follow="pattren error"
+		try:
+			address=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div[2]/div[3]/div/div[2]/div[1]').get_attribute("innerText")
+		except NoSuchElementException:
+			address="pattern error"
+		try:
+			contact=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div[2]/div[4]/div/div[2]/div').get_attribute("innerText")
+		except NoSuchElementException:
+			contact="pattern error"
+		try:
+			clgname=browser.find_element_by_class_name('_64-f').get_attribute("innerText")
+		except NoSuchElementException:
+			clgname="pattern error"
                 filewriter.writerow([clgname,address,contact,rating,likes,follow])
                 print("Data scrapped from "+ clgname +" in csv file")
 
