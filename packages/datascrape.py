fc49f50bd0b87cafc698559a183257538eb5747c
Rabin Joshi <41543258+rabeenj007@users.noreply.github.com>
2019-01-02 17:12:42 +0545

    Update datascrape.py

--- packages/datascrape.py
+++ packages/datascrape.py
@@ -18,7 +18,7 @@ def scrape_data():
                 try:
                     follow=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[3]/div/div[1]/div[3]/div/div[2]/div').get_attribute("innerText")
                 except NoSuchElementException:
-                    follow="pattren error"
+                    follow="pattern error"
                 try:
                     address=browser.find_element_by_xpath('/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div/div[1]/div/div/div[3]/div/div[2]/div[3]/div/div[2]/div[1]').get_attribute("innerText")
                 except NoSuchElementException:
