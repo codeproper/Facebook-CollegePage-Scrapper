09e4510bcc2f725e8be2e13f50d5e6c5370fd89f
Rabin Joshi <41543258+rabeenj007@users.noreply.github.com>
2019-01-02 17:03:14 +0545

    Update _init_.py

--- packages/_init_.py
+++ packages/_init_.py
@@ -9,7 +9,6 @@ from more_itertools import unique_everseen
 ###IMPORTING SUPPORTING FILES
 from .datascrape import scrape_data
 from .findpages  import findpages
-from .urlscrape import scrape_urls
 
 
 ### Specifying incognito mode as you launch your browser[OPTIONAL]
@@ -17,17 +16,8 @@ option = webdriver.ChromeOptions()
 option.add_argument("--incognito")
 browser = webdriver.Chrome(executable_path='./chromedriver')
 
-#####username and password can be defined here to automate the process
-#Username=""
-#Userpass=""
-
-print("A dummy fb id may be suitable for this purpose")
-Username=input("Enter the username:")
-Userpass=input("Enter the password:")
-####Handles login and page search for list of colleges
-link_list,num_pages=findpages(Username,Userpass)
 ####Store the page urls in csv file####
-scrape_urls(link_list,num_pages)
+findpages()
 ####Open the url.csv file to begin scraping from pages and stores the result in final.csv file####
 scrape_data()
 print("Its done.Check out the scrapped urls in url.csv and data in finaldata.csv!!")
