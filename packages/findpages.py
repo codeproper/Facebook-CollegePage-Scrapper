d0dab8c8d9542acba712d2a2890559533afe8b8d
Rabin Joshi <41543258+rabeenj007@users.noreply.github.com>
2019-01-02 16:59:07 +0545

    Update to make it run without logging FB

--- packages/findpages.py
+++ packages/findpages.py
@@ -1,20 +1,19 @@
-def findpages(username,password):
-    browser.get("https://www.facebook.com")
-    print ("Opened facebook...")
-    sleep(1)
-    browser.find_element_by_id('email').send_keys(username)
-    browser.find_element_by_name('pass').send_keys(password)
-    browser.find_element_by_id('loginbutton').click()
-    print ("Logged in FB...")
-    sleep(3)
-    print("Opening page of colleges nepal...")
-    browser.get("https://www.facebook.com/search/pages/?q=colleges%20nepal&epa=SERP_TAB")
-    print("Stop all running browser before starting...")
-    scroll_num=input("How many time to scroll the page[More scroll= More pages]:")
-    scroll_num=int(scroll_num)
-    for m in range(scroll_num):
-         browser.find_element_by_tag_name('html').send_keys(Keys.END)
-         sleep(3)
-    link_list=browser.find_elements_by_class_name('_32mo')
-    num_pages=len(link_list)
-    return link_list,num_pages
+def findpages():
+    browser.get('https://www.facebook.com/public?query=colleges%20nepal&type=pages&init=dir&nomc=0')
+    with open('url.csv', 'w') as File:
+        filewriter = csv.writer(File, delimiter=',',quoting=csv.QUOTE_MINIMAL)
+        scroll_num=input("How many time to scroll the page[More scroll= More pages]:")
+    #### To automate the process just define value of scroll num
+    #scroll_num=
+        scroll_num=int  (scroll_num)
+
+        for m in range(scroll_num):
+            browser.find_element_by_tag_name('html').send_keys(Keys.END)
+            sleep(3)
+        links=browser.find_elements_by_class_name('_32mo')#.get_attribute("href")
+        for i in range(len(links)):
+            link=links[i].get_attribute("href")
+            filewriter.writerow([link])
+        print("Done writing urls in url.csv file.. ")
+
+    
