ba8d60d07c0d9726aa4ca7bc8028390747e3ba5f
Rabin Joshi <rabeenj10@gmail.com>
2019-01-31 15:30:37 +0545

    Delete scrape.py

--- scrape.py
+++
@@ -1,9 +0,0 @@
-###IMPORTING SUPPORTING FILES
-from datascrape import scrape_data
-from findpages  import findpages
-
-####Store the page links in url.csv file####
-findpages()
-####Open the url.csv file to begin scraping from pages and store the result in final.csv file####
-scrape_data()
-print("Its done.Check out the scrapped urls in url.csv and data in data.csv!!")
