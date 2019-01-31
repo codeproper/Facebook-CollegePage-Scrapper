0ed9d76589a744d179c6a3cff43b86fbd8881a33
rabeenj007 <rabeenj10@gmail.com>
2019-01-31 17:22:50 +0545

    Add data entry in single loop

--- review.py
+++ review.py
@@ -4,14 +4,12 @@ import csv
 import urllib.request
 from bs4 import BeautifulSoup
 from  more_itertools import unique_everseen
-import re
-#from selenium.common.exceptions import NoSuchElementException
 option = webdriver.ChromeOptions()
 option.add_argument("--incognito")
 #option.add_argument("--headless")
 browser = webdriver.Chrome(executable_path='/bin/chromedriver', options=option)
 
-def scrape_data():
+def review_data():
 
     with open('url.csv') as csvfile:
         urlreader = csv.reader(csvfile, delimiter=',', quotechar=',',quoting=csv.QUOTE_MINIMAL)
@@ -22,46 +20,45 @@ def scrape_data():
 
             browser.get(review_url)
             #data=browser.find_elements_by_id('fwb')#.get_attribute("href")
-            data= soup.find_all("span", attrs={"class":"fcg"})
+            review_data= soup.find_all("span", attrs={"class":"fcg"})
             reviews= soup.find_all("p")
-            text_data=[]
-            review_data=[]
+            unique_review_data=[]
+            review_text=[]
+            temp_review_data=[]
 
-            for u in range(len(data)):
-                rere= data[u].text
-                text_data.append(rere)
-            data1=list(unique_everseen(text_data))
-            for i in range(len(reviews)):
-                fefe = reviews[i].text
-                review_data.append(fefe)
-            a= 0
-            b= 1
-            for f in range(len(data1)):
-
-                if a<len(data1):
+            for u in range(len(review_data)):
+                rere = review_data[u].text
+                temp_review_data.append(rere)
+            unique_review_data = list(unique_everseen(temp_review_data))
+            for f in range(len(reviews)):
+                fefe = reviews[f].text
+                review_text.append(fefe)
+            name_index = 0
+            date_index = 1
+            for o in range(len(unique_review_data)):
+                if name_index < len(unique_review_data):
                     try:
-                        jj = data1[a].split("reviewed")
-                        reviewer_name=jj[0]
-                        gg=jj[1].split("—")
-                        review_clg=gg[0]
-                        review_rating=gg[1]
-
-                        review_date = data1[b]
+                        jj = unique_review_data[name_index].split("reviewed")
+                        reviewer_name = jj[0]
+                        gg = jj[1].split("—")
+                        review_clg = gg[0]
+                        review_rating = gg[1]
+                        review_date = unique_review_data[date_index]
+                        try:
+                            review_text = reviews[o].text
+                        except:
+                            review_text = "No Review"
                         print(reviewer_name)
                         print(review_clg)
                         print(review_rating)
                         print(review_date)
-                        a=a+2
-                        b=b+2
+                        print(review_text)
+                        name_index = name_index+2
+                        date_index = date_index+2
                     except:
                         continue
                 else:
                     break
 
-
-            for o in range(len(review_data)):
-                print(review_data[o])
-
-
-scrape_data()
+review_data()
 
